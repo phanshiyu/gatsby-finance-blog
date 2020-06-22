c99fc44b44ea51c9154e618b38167c4931e8db2a
Phan Shi Yu <phanshiyu@gmail.com>
2020-06-22 11:30:08 +0800

    Cleaned up starter/boilerplate

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,14 +1,14 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Starter Blog`,
+    title: `Finance Guide`,
     author: {
-      name: `Kyle Mathews`,
-      summary: `who lives and works in San Francisco building useful things.`,
+      name: `Guang Yuan and Shi Yu`,
+      summary: ``,
     },
-    description: `A starter blog demonstrating what Gatsby can do.`,
-    siteUrl: `https://gatsby-starter-blog-demo.netlify.app/`,
+    description: `Singapore Basic Finance`,
+    siteUrl: ``,
     social: {
-      twitter: `kylemathews`,
+      twitter: ``,
     },
   },
   plugins: [
---
+++ src/utils/colors.js
@@ -0,0 +1,7 @@
+export default {
+    blackCoffee: "#483d3f",
+    greenBlueCrayola: "#058ed9",
+    eggShell: "#f4ebd9",
+    spanishGray: "#A39A92",
+    dimGray: "#77685d"
+}
